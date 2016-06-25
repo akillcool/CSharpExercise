ece99a465321c19be4914a93c13fa2872ed7d401
akillcool <akillcool@outlook.com>
2016-06-25 23:35:42 +0800

    超EZ的99乘法表

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -1,4 +1,4 @@
-﻿//try catch finally
+﻿//超EZ的99乘法表
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,56 +13,18 @@ namespace CSharpExercise
 
         static void Main(string[] args)
         {
-            Calculator c = new Calculator();
-            int result = 0;
-            try
+            for (int a = 1; a <= 9; a++)
             {
-                result = c.Add(null, "200");
+                for (int b = 1; b <= a; b++)
+                {
+                    //if (b > a)
+                    //{
+                    //    break;
+                    //}
+                    Console.Write("{0} X {1} = {2}\t", a, b, a * b);
+                }
+                Console.WriteLine();
             }
-            catch (ArgumentNullException ane)
-            {
-                Console.WriteLine(ane.Message);
-            }
-            Console.WriteLine(result);
-        }
-    }
-    class Calculator
-    {
-        public int Add(string arg1, string arg2)
-        {
-            int a = 0;
-            int b = 0;
-            //bool hasError = false;
-            try
-            {
-                a = int.Parse(arg1);
-                b = int.Parse(arg2);
-            }
-            catch (ArgumentNullException ane)
-            {
-                //hasError = true;
-                //Console.WriteLine(ane.Message);
-                throw ane;
-            }
-            catch(FormatException fe)
-            {
-                //hasError = true;
-                Console.WriteLine(fe.Message);
-            }
-            catch(OverflowException ofe)
-            {
-                //hasError = true;
-                Console.WriteLine(ofe.Message);
-            }
-            //finally
-            //{
-            //    if (hasError)
-            //    {
-            //        Console.WriteLine("Something happened.");
-            //    }
-            //}
-            int result = checked(a + b);
-            return result;
         }
     }
 }
