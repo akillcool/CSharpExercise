8cb507c5e7c788fb0759487ff876d510c1f776aa
akillcool <akillcool@outlook.com>
2016-06-26 23:32:08 +0800

    委托 模板方法

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -1,4 +1,4 @@
-﻿//自定义委托
+﻿//委托 模板方法
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,55 +7,60 @@ using System.Threading.Tasks;
 
 namespace CSharpExercise
 {
-    public delegate double Calc(double a, double b);
-
     class Program
     {
 
         static void Main(string[] args)
         {
-            Calculator calculator = new Calculator();
-            Calc calc1 = new Calc(calculator.Add);
-            Calc calc2 = new Calc(calculator.Sub);
-            Calc calc3 = new Calc(calculator.Mul);
-            Calc calc4 = new Calc(calculator.Div);
+            ProductFactory productFactory = new ProductFactory();
+            WrapFactory wrapFactory = new WrapFactory();
 
-            double a = 100;
-            double b = 200;
-            double c = 0;
+            Func<Product> func1 = new Func<Product>(productFactory.MakePizza);
+            Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
 
-            c = calc1(a, b);
-            Console.WriteLine(c);
-            c = calc2(a, b);
-            Console.WriteLine(c);
-            c = calc3(a, b);
-            Console.WriteLine(c);
-            c = calc4(a, b);
-            Console.WriteLine(c);
+            Box box1 = wrapFactory.WrapProduct(func1);
+            Box box2 = wrapFactory.WrapProduct(func2);
+
+            Console.WriteLine(box1.Product.Name);
+            Console.WriteLine(box2.Product.Name);
         }
     }
 
-    class Calculator
+    class Product
     {
-        public double Add(double a, double b)
-        {
-            return a + b;
-        }
+        public string Name { get; set; }
+    }
 
-        public double Sub(double a, double b)
-        {
-            return a - b;
-        }
+    class Box
+    {
+        public Product Product { get; set; }
+    }
 
-        public double Mul(double a, double b)
+    class WrapFactory   //模板方法
+    {
+        public Box WrapProduct(Func<Product> getProduct)
         {
-            return a * b;
-        }
-
-        public double Div(double a, double b)
-        {
-            return a / b;
+            Box box = new Box();
+            Product product = getProduct.Invoke();
+            box.Product = product;
+            return box;
         }
     }
 
+    class ProductFactory
+    {
+        public Product MakePizza()
+        {
+            Product product = new Product();
+            product.Name = "Pizza";
+            return product;
+        }
+
+        public Product MakeToyCar()
+        {
+            Product product = new Product();
+            product.Name = "Toy Car";
+            return product;
+        }
+    }
 }
