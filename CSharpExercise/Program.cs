0eb9adfbd90f62520a8bceed3556f106cabe1fa9
akillcool <akillcool@outlook.com>
2016-06-28 10:26:10 +0800

    使用接口来取代委托

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -1,9 +1,8 @@
-﻿//显式异步调用 Task
+﻿//使用接口来取代委托
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpExercise
@@ -13,41 +12,61 @@ namespace CSharpExercise
 
         static void Main(string[] args)
         {
-            Student stu1 = new Student() { ID = 1, PenColor = ConsoleColor.Yellow };
-            Student stu2 = new Student() { ID = 2, PenColor = ConsoleColor.Green };
-            Student stu3 = new Student() { ID = 3, PenColor = ConsoleColor.Red };
+            IProductFactory pizzaFactory = new PizzaFactory();
+            IProductFactory toyCarFactory = new ToyCarFactory();
+            WrapFactory wrapFactory = new WrapFactory();
 
-            Task task1 = new Task(new Action(stu1.DoHomework));
-            Task task2 = new Task(new Action(stu2.DoHomework));
-            Task task3 = new Task(new Action(stu3.DoHomework));
+            Box box1 = wrapFactory.WrapProduct(pizzaFactory);
+            Box box2 = wrapFactory.WrapProduct(toyCarFactory);
 
-            task1.Start();
-            task2.Start();
-            task3.Start();
-
-
-            for (int i = 0; i < 10; i++)
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("Main thread {0}",i);
-                Thread.Sleep(1000);
-            }
+            Console.WriteLine(box1.Product.Name);
+            Console.WriteLine(box2.Product.Name);
         }
     }
 
-    class Student
+    interface IProductFactory
     {
-        public int ID { get; set; }
-        public ConsoleColor PenColor { get; set; }
+        Product Make();
+    }
 
-        public void DoHomework()
+    class PizzaFactory : IProductFactory
+    {
+        public Product Make()
         {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.ForegroundColor = this.PenColor;
-                Console.WriteLine("Student {0} doing homework {1} hour(s).", this.ID, i);
-                Thread.Sleep(1000);
-            }
+            Product product = new Product();
+            product.Name = "Pizza";
+            return product;
+        }
+    }
+
+    class ToyCarFactory : IProductFactory
+    {
+        public Product Make()
+        {
+            Product product = new Product();
+            product.Name = "Toy Car";
+            return product;
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+    }
+
+    class Box
+    {
+        public Product Product { get; set; }
+    }
+
+    class WrapFactory   //模板方法
+    {
+        public Box WrapProduct(IProductFactory productFactory)
+        {
+            Box box = new Box();
+            Product product = productFactory.Make();
+            box.Product = product;
+            return box;
         }
     }
 }
