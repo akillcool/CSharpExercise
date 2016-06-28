882fbf42bb35a8bf472982c91f8f6f55ef1e1d07
akillcool <akillcool@outlook.com>
2016-06-28 10:08:00 +0800

    使用委托进行隐式异步调用 BeginInvoke

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -1,4 +1,4 @@
-﻿//多播（multicast）委托
+﻿//使用委托进行隐式异步调用 BeginInvoke
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,19 +16,22 @@ namespace CSharpExercise
             Student stu1 = new Student() { ID = 1, PenColor = ConsoleColor.Yellow };
             Student stu2 = new Student() { ID = 2, PenColor = ConsoleColor.Green };
             Student stu3 = new Student() { ID = 3, PenColor = ConsoleColor.Red };
+
             Action action1 = new Action(stu1.DoHomework);
             Action action2 = new Action(stu2.DoHomework);
             Action action3 = new Action(stu3.DoHomework);
 
-            //单播委托，一个委托封装一个方法
-            //action1.Invoke();
-            //action2.Invoke();
-            //action3.Invoke(); 
+            action1.BeginInvoke(null, null);
+            action2.BeginInvoke(null, null);
+            action3.BeginInvoke(null, null);
 
-            action1 += action2;
-            action1 += action3;
 
-            action1.Invoke();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Main thread {0}",i);
+                Thread.Sleep(1000);
+            }
         }
     }
 
