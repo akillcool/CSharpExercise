83a5570999b8bea9aafac5b5377511409520d07e
akillcool <akillcool@outlook.com>
2016-06-28 09:50:31 +0800

    多播（multicast）委托

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -1,8 +1,9 @@
-﻿//委托 回调方法
+﻿//多播（multicast）委托
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpExercise
@@ -12,74 +13,38 @@ namespace CSharpExercise
 
         static void Main(string[] args)
         {
-            ProductFactory productFactory = new ProductFactory();
-            WrapFactory wrapFactory = new WrapFactory();
+            Student stu1 = new Student() { ID = 1, PenColor = ConsoleColor.Yellow };
+            Student stu2 = new Student() { ID = 2, PenColor = ConsoleColor.Green };
+            Student stu3 = new Student() { ID = 3, PenColor = ConsoleColor.Red };
+            Action action1 = new Action(stu1.DoHomework);
+            Action action2 = new Action(stu2.DoHomework);
+            Action action3 = new Action(stu3.DoHomework);
 
-            Func<Product> func1 = new Func<Product>(productFactory.MakePizza);
-            Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
+            //单播委托，一个委托封装一个方法
+            //action1.Invoke();
+            //action2.Invoke();
+            //action3.Invoke(); 
 
-            Logger logger = new Logger();
-            Action<Product> log = new Action<Product>(logger.Log);
+            action1 += action2;
+            action1 += action3;
 
-            Box box1 = wrapFactory.WrapProduct(func1,log);
-            Box box2 = wrapFactory.WrapProduct(func2,log);
-
-            Console.WriteLine(box1.Product.Name);
-            Console.WriteLine(box2.Product.Name);
+            action1.Invoke();
         }
     }
 
-    class Logger
+    class Student
     {
-        public void Log(Product product)
+        public int ID { get; set; }
+        public ConsoleColor PenColor { get; set; }
+
+        public void DoHomework()
         {
-            Console.WriteLine("Product '{0}' created at {1}, price is {2}.", product.Name, DateTime.UtcNow, product.Price);
-        }
-    }
-
-    class Product
-    {
-        public string Name { get; set; }
-        public double Price { get; set; }
-    }
-
-    class Box
-    {
-        public Product Product { get; set; }
-    }
-
-    class WrapFactory
-    {
-        public Box WrapProduct(Func<Product> getProduct, Action<Product> logCallback)
-        {
-            Box box = new Box();
-            Product product = getProduct.Invoke();
-            if (product.Price >= 50)
+            for (int i = 0; i < 5; i++)
             {
-                logCallback(product);
+                Console.ForegroundColor = this.PenColor;
+                Console.WriteLine("Student {0} doing homework {1} hour(s).", this.ID, i);
+                Thread.Sleep(1000);
             }
-
-            box.Product = product;
-            return box;
-        }
-    }
-
-    class ProductFactory
-    {
-        public Product MakePizza()
-        {
-            Product product = new Product();
-            product.Name = "Pizza";
-            product.Price = 12;
-            return product;
-        }
-
-        public Product MakeToyCar()
-        {
-            Product product = new Product();
-            product.Name = "Toy Car";
-            product.Price = 100;
-            return product;
         }
     }
 }
