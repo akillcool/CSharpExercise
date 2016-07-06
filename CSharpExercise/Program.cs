91f6dc74d01629cf079384313f6743490d466687
akillcool <akillcool@outlook.com>
2016-07-06 22:48:50 +0800

    Event Sample 3

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -1,4 +1,4 @@
-﻿//Event Sample 2
+﻿//Event Sample 3
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,29 +11,18 @@ namespace CSharpExercise
     {
         static void Main(string[] args)
         {
-            Form form = new Form();
-            Controller controller = new Controller(form);
+            MyForm form = new MyForm();
 
-            form.ShowDialog();      //防止程序跳出
+            form.Click += form.Action;
+            form.ShowDialog();
         }
+    }
 
-        class Controller
+    class MyForm : Form
+    {
+        internal void Action(object sender, EventArgs e)
         {
-            private Form form;
-
-            public Controller(Form form)    //构造器
-            {
-                if (form != null)
-                {
-                    this.form = form;
-                    this.form.Click += FormClicked;
-                }
-            }
-
-            private void FormClicked(object sender, EventArgs e)
-            {
-                this.form.Text = DateTime.Now.ToString();
-            }
+            this.Text = DateTime.Now.ToString();
         }
     }
 }
