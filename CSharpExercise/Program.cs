13e6b40e6b084729523eb8181752740eeebf109a
akillcool <akillcool@outlook.com>
2016-07-06 22:20:10 +0800

    Event Sample 1

--- CSharpExercise/Program.cs
+++ CSharpExercise/Program.cs
@@ -1,72 +1,41 @@
-﻿//使用接口来取代委托
+﻿//Event Sample 1
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 
 namespace CSharpExercise
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            IProductFactory pizzaFactory = new PizzaFactory();
-            IProductFactory toyCarFactory = new ToyCarFactory();
-            WrapFactory wrapFactory = new WrapFactory();
+            Timer timer = new Timer();
+            Boy boy = new Boy();
+            Girl girl = new Girl();
 
-            Box box1 = wrapFactory.WrapProduct(pizzaFactory);
-            Box box2 = wrapFactory.WrapProduct(toyCarFactory);
-
-            Console.WriteLine(box1.Product.Name);
-            Console.WriteLine(box2.Product.Name);
+            timer.Interval = 1000;
+            timer.Elapsed += boy.Action;
+            timer.Elapsed += girl.Action;
+            timer.Start();
+            Console.ReadLine();     //等待读取，让事件响应
         }
-    }
 
-    interface IProductFactory
-    {
-        Product Make();
-    }
-
-    class PizzaFactory : IProductFactory
-    {
-        public Product Make()
+        class Boy
         {
-            Product product = new Product();
-            product.Name = "Pizza";
-            return product;
+            internal void Action(object sender, ElapsedEventArgs e)
+            {
+                Console.WriteLine("Jump!");
+            }
         }
-    }
 
-    class ToyCarFactory : IProductFactory
-    {
-        public Product Make()
+        class Girl
         {
-            Product product = new Product();
-            product.Name = "Toy Car";
-            return product;
-        }
-    }
-
-    class Product
-    {
-        public string Name { get; set; }
-    }
-
-    class Box
-    {
-        public Product Product { get; set; }
-    }
-
-    class WrapFactory   //模板方法
-    {
-        public Box WrapProduct(IProductFactory productFactory)
-        {
-            Box box = new Box();
-            Product product = productFactory.Make();
-            box.Product = product;
-            return box;
+            internal void Action(object sender, ElapsedEventArgs e)
+            {
+                Console.WriteLine("Sing!");
+            }
         }
     }
 }
